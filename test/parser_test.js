4944520c093c5e5360242e5bc9f5544e0663b99b
Jack Matthews <jackhkmatthews@gmail.com>
2017-06-22 11:57:59 +0100

    get formation names

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,31 +1,45 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 
-describe('getFormations', function() {
-  const getFormations = require('../lib/parser-methods.js').getFormations;
+describe('getFormationStrings', function() {
+  const getFormationStrings = require('../lib/parser-methods.js').getFormationStrings;
 
-  const filePath = `${__dirname}/week.txt`;
+  const inputString = `11/04/2016: ken > alex > fred > ryan > greg > ed > chris > radek > florence > vanda | alex > ed > florence > vanda > ryan > greg > ken > chris > radek > fred
 
-  // beforeEach(function(done){
-  //   fs.readFile(filePath, 'utf8', function(err, fileAsString) {
-  //     if (err) throw err;
-  //     string = fileAsString;
-  //     done();
-  //   });
-  // });
+12/04/2016: ken > fred > alex > chris > ryan > ed > radek > james > vanda > florence > greg | alex > james > ryan > florence >fred > chris > ken > ed > radek > greg > vanda
 
-  it('should return an array', function() {
+13/04/2016: ken > vanda > james > ed > radek > chris > ryan > greg | greg > ryan > chris > radek > ed > james > vanda > ken
 
-    let string;   
+14/04/2016: ken > fred > alex > ryan > chris > james > ed > radek > florence > vanda > greg | vanda > chris > florence > ryan > ed > fred > greg > ken > james > radek > alex
 
-    fs.readFile(filePath, 'utf8', function(err, fileAsString) {
-      if (err) throw err;
-      string = fileAsString;
-      // done();
-      expect(getFormations(string).to.be.an('array'));
-    });
+15/04/2016: ken > fred > ryan > vanda > ed > alex > chris > james > florence > greg > radek | alex > ed > vanda > ryan > fred > ken > radek > greg > florence > james > chris`;
+  
+  const outputArray = [ 
+    ': ken > alex > fred > ryan > greg > ed > chris > radek > florence > vanda |',
+    ': ken > fred > alex > chris > ryan > ed > radek > james > vanda > florence > greg |',
+    ': ken > vanda > james > ed > radek > chris > ryan > greg |',
+    ': ken > fred > alex > ryan > chris > james > ed > radek > florence > vanda > greg |',
+    ': ken > fred > ryan > vanda > ed > alex > chris > james > florence > greg > radek |' 
+  ];
 
-    // expect(getFormations(string).to.be.an('array'));
+  it('should correctly convert string to array of formations from each day', function() {
+    expect(getFormationStrings(inputString)).to.be.an('array');
+    expect(getFormationStrings(inputString).length).to.equal(5);
+    expect(getFormationStrings(inputString)).to.deep.equal(outputArray);
   });
 
+});
+
+describe('getFormationNames', function() {
+  const getFormationNames = require('../lib/parser-methods.js').getFormationNames;
+
+  const inputString = ': ken > alex > fred > ryan > greg > ed > chris > radek > florence > vanda |';
+
+  const outputArray = ['ken', 'alex', 'fred', 'ryan', 'greg', 'ed', 'chris', 'radek', 'florence', 'vanda'];
+
+  it('should convert formation string into an array of names', function() {
+    expect(getFormationNames(inputString)).to.be.an('array');
+    expect(getFormationNames(inputString).length).to.equal(10);
+    expect(getFormationNames(inputString)).to.deep.equal(outputArray);
+  });
 });
